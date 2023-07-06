335ff173e242d531a0485917301a79be2687f7a1
phptuts <glaserpower@gmail.com>
2023-07-05 22:15:42 -0700

    2-sequelize-dotenv-setup

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const userRoute = require("./routes/user.route");
+const dotenv = require("dotenv");
+dotenv.config();
+const { sequelize } = require("./database/db");
 const app = express();
 app.use(express.json());
 
 app.use("/users", userRoute);
 
-app.listen(3000, () => {
-  console.log("working");
+app.listen(3000, async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("working");
+  } catch (e) {
+    console.log("error", e);
+  }
 });
