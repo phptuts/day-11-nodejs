61e1238f638c5bb0abcbac54410488288c4fe62c
phptuts <glaserpower@gmail.com>
2023-07-13 21:30:20 -0700

    2-jwt-setup

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const dotenv = require("dotenv");
 dotenv.config();
-const { sequelize } = require("./database/db");
+const { sequelize, UserModel } = require("./database/db");
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const userRoute = require("./routes/user.route");
 const app = express();
 app.use(express.json());
@@ -12,6 +13,14 @@ app.listen(3000, async () => {
   try {
     await sequelize.authenticate();
     await sequelize.sync({ force: true });
+    const user = await UserModel.create({
+      email: "b@gmail.com",
+      password: "password",
+    });
+    const payload = { userId: user.id };
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "4h",
+    });
     console.log("working");
   } catch (e) {
     console.log("error", e);
